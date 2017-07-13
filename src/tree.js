ba9a9c3baeb1f59af226d86f3f403a4a67684d4d
jeshsy <jeshsy@users.noreply.github.com>
2017-07-13 16:05:23 -0400

    got most of tree to work

--- src/tree.js
+++ src/tree.js
@@ -3,6 +3,19 @@ class Tree {
     this.value = value;
     this.children = [];
   }
+  addChild(value) {
+    const node = new Tree(value);
+    this.children.push(node);
+  }
+  contains(value) {
+    if (this.value === value) return true;
+    for (let i = 0; i < this.children.length; i++) {
+      if (this.children.length === 0) return false;
+      // return this.children[i].contains(value);
+      if (this.children[i].value === value) return true;
+      return false;
+    }
+  }
 }
-
 module.exports = Tree;
+
