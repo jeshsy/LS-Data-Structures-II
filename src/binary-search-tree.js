a40f0cf76d17f048c985fe04d9a42b1fe93557ed
jeshsy <jeshsy@users.noreply.github.com>
2017-07-14 17:24:58 -0400

    got bst to pass all tests

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -5,6 +5,26 @@ class BinarySearchTree {
     this.left = null;
     this.right = null;
   }
+  insert(value) {
+    const node = new BinarySearchTree(value);
+    if (value < this.value && this.left === null) this.left = node;
+    if (value < this.value) this.left.insert(value);
+    if (value > this.value && this.right === null) this.right = node;
+    if (value > this.value) this.right.insert(value);
+  }
+  contains(value) {
+    let x = 0;
+    if (this.value === value) x += 1;
+    if (this.left !== null) x += this.left.contains(value);
+    if (this.right !== null) x += this.right.contains(value);
+    return x > 0;
+  }
+  depthFirstForEach(cb) {
+    const arr = [];
+    arr.push(cb(this.value));
+    if (this.left !== null) arr.push(this.left.depthFirstForEach(cb));
+    if (this.right !== null) arr.push(this.right.depthFirstForEach(cb));
+  }
 }
 
 module.exports = BinarySearchTree;
